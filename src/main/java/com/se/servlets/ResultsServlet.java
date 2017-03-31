3f688d60491beda9a7c38a18e3ff3099f12e4dd1
Vivek <vivekpn.93@gmail.com>
2017-03-31 04:35:28 -0700

    Load the page directly from the results page.

--- src/main/java/com/se/servlets/ResultsServlet.java
+++ src/main/java/com/se/servlets/ResultsServlet.java
@@ -13,42 +13,37 @@ import javax.servlet.http.HttpServletResponse;
 import com.se.data.SearchResult;
 import com.se.query.QueryRunner;
 
-/**
- * Servlet implementation class secondservlet
- */
 public class ResultsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public ResultsServlet() {
 		super();
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 
 		String query = request.getParameter("search");
+
+		if (query == null) {
+			query = "";
+		}
+
 		QueryRunner queryRunner = new QueryRunner();
 		List<SearchResult> searchResults = queryRunner.search(query);
+		List<SearchResultsUI> searchResultsUIs = new ArrayList<>();
+		for (SearchResult searchResult : searchResults) {
+			searchResultsUIs.add(new SearchResultsUI(searchResult));
+		}
 
-		request.setAttribute("searchResults", searchResults);
+		request.setAttribute("searchResultsUIs", searchResultsUIs);
 		request.setAttribute("query", query);
 		request.getRequestDispatcher("search.jsp").forward(request, response);
 
 		out.close();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
@@ -64,7 +59,7 @@ public class ResultsServlet extends HttpServlet {
 		request.setAttribute("searchResultsUIs", searchResultsUIs);
 		request.setAttribute("query", query);
 		request.getRequestDispatcher("search.jsp").forward(request, response);
-		
+
 		out.close();
 	}
 
